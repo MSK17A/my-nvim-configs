59ceae1c90945a63a7c939e106a1bf9eb9b104a5
Mohammed AlSammak <msk170a@gmail.com>
2024-01-25 16:01:45 +0300

    .

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,39 @@
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html", "cssls", "clangd", "gopls", "tsserver", "rust_analyzer" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- configure pyright
+lspconfig["pyright"].setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "off",
+      },
+    },
+  },
+}
+
+-- Without the loop, you would have to manually set up each LSP 
+-- 
+-- lspconfig.html.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+--
+-- lspconfig.cssls.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+--
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,26 @@
+local plugins = {
+
+-- In order to modify the `lspconfig` configuration:
+{
+  "neovim/nvim-lspconfig",
+   config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+   end,
+},
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"go", "javascript", "javascriptreact", "typescript", "typescriptreact", "python"},
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end
+  },
+  -- {
+  --   'mfussenegger/nvim-lint',
+  -- },
+  {
+    'Vimjas/vim-python-pep8-indent',
+  },
+}
+
+return plugins

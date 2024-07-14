ae8cd16a520c3a04db1b9b322a1485f75edc1721
Mohammed AlSammak <msk170a@gmail.com>
2024-07-14 08:32:21 +0300

    .

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false,
+})
+vim.g.copilot_no_tab_map = true

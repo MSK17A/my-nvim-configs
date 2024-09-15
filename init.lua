f302e2a37128307d27909a93008501466c5bbda2
Mohammed AlSammak <msk170a@gmail.com>
2024-09-15 22:52:00 +0300

    .

--- init.lua
+++ init.lua
@@ -5,4 +5,10 @@ vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("colorscheme catppuccin-frappe") --catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha"
 
+-- Set absolute line numbers
+vim.opt.number = true
+
+-- Set relative line numbers
+vim.opt.relativenumber = true
+
 vim.keymap.set('n', '<leader>T', function() vim.cmd("Telescope") end)
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,8 +5,10 @@ return {
 		null_ls.setup({
       -- TODO: Find a way to automate this (take formatters from mason and loop)
 			sources = {
+        null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.eslint_d,
 			},
 		})
 		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format code" })

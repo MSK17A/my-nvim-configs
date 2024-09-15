47ec4d8e730dd2f6dd505e13c9fd305fa623f56b
Mohammed AlSammak <msk170a@gmail.com>
2024-09-16 02:06:10 +0300

    .

--- init.lua
+++ init.lua
@@ -4,11 +4,16 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("colorscheme catppuccin-frappe") --catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha"
+vim.wo.signcolumn = "yes"
+vim.cmd("set cursorline")
+vim.opt.ruler = true
+vim.opt.breakindent = true
 
 -- Set absolute line numbers
 vim.opt.number = true
-
 -- Set relative line numbers
 vim.opt.relativenumber = true
 
-vim.keymap.set('n', '<leader>T', function() vim.cmd("Telescope") end)
+vim.keymap.set("n", "<leader>T", function()
+	vim.cmd("Telescope")
+end)

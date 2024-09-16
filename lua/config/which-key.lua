1e04bcd9745a398a42bfe3305cd6a2a6d9411740
Mohammed AlSammak <msk170a@gmail.com>
2024-09-16 11:21:33 +0300

    no need

--- lua/config/which-key.lua
+++
@@ -1,7 +0,0 @@
-local wk = require("which-key")
-
--- Register your key mappings
-wk.register({
-  ["<leader>T"] = { "<cmd>Telescope find_files<cr>", "󰱼 Find Files" }, -- Add an icon to the description
-})
-
--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -9,34 +9,6 @@ return {
 		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
 		config = true,
 	},
-	{ -- Remind yourself with your Keymaps
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-			{
-				"<leader>T",
-				"<cmd>Telescope find_files<cr>",
-				desc = "Find Files",
-			},
-			{
-				"<leader>e",
-				"<cmd>Neotree toggle<cr>",
-				desc = "Explore Files",
-			},
-		},
-	},
 	{ -- For rainbow indent.
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",

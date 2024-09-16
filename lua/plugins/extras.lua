982336c75ce255ef574e1f84eadf6da45b0cfaa2
Mohammed AlSammak <msk170a@gmail.com>
2024-09-16 10:15:25 +0300

    this was changed to extras

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,11 +1,15 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{
+	{ -- Your favorite colorscheme.
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{ -- neovim healthcheck thorw an error on me becasue this is not installed.
 		"vhyrro/luarocks.nvim",
 		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
 		config = true,
 	},
-	{
+	{ -- Remind yourself with your Keymaps
 		"folke/which-key.nvim",
 		event = "VeryLazy",
 		opts = {
@@ -33,15 +37,21 @@ return {
 			},
 		},
 	},
-	{
+	{ -- For rainbow indent.
 		"lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
+		main = "ibl",
 		opts = function(_, opts)
-			-- Other blankline configuration here
+			-- Other blankline configuration here e
 			return require("indent-rainbowline").make_opts(opts)
 		end,
 		dependencies = {
 			"TheGLander/indent-rainbowline.nvim",
 		},
+	},
+	{ -- For gitlens like.
+		"APZelos/blamer.nvim",
+		config = function()
+			vim.cmd("BlamerShow")
+		end,
 	},
 }

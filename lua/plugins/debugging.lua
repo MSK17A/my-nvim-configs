880161ae8da824cc5f813000adf01c24e257a53a
Mohammed AlSammak <msk170a@gmail.com>
2024-09-16 11:22:21 +0300

    debugging bindings

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,31 +1,38 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
-    "leoluz/nvim-dap-go",
-  },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"leoluz/nvim-dap-go",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-    require("dapui").setup()
-    require("dap-go").setup()
+		require("dapui").setup()
+		require("dap-go").setup()
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
 
-    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "toggle breakpoint" })
-    vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "continue" })
-  end,
+		local wk = require("which-key")
+		-- Register your key mappings
+		wk.add({
+			{ "<leader>d", group = "Debug" }, -- group
+		})
+
+		vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, { desc = "toggle breakpoint" })
+		vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "continue" })
+		vim.keymap.set("n", "<Leader>dC", dap.clear_breakpoints, { desc = "Clear breakpoints" })
+	end,
 }

a0d1e12340a21a72806e2b40cb364b4015f2fd0a
Mohammed AlSammak <msk170a@gmail.com>
2024-09-19 11:17:32 +0300

    toggle term configs

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,18 +1,22 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    cmd = "ToggleTerm",
-    build = ":ToggleTerm",
-    keys = { { "<F4>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
-    opts = {
-      open_mapping = [[<F4>]],
-      direction = "horizontal",
-      shade_filetypes = {},
-      hide_numbers = true,
-      insert_mappings = true,
-      terminal_mappings = true,
-      start_in_insert = true,
-      close_on_exit = true,
+  "akinsho/toggleterm.nvim",
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<c-\>]],
+      shade_terminals = false,
+      -- add --login so ~/.zprofile is loaded
+      -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
+      shell = "zsh --login",
+    })
+  end,
+  keys = {
+    { [[<C-\>]] },
+    { "<leader>t", desc = "Terminal" },
+    { "<leader>tt", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
+    {
+      "<leader>td",
+      "<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>",
+      desc = "Open a horizontal terminal at the Desktop directory",
     },
   },
 }

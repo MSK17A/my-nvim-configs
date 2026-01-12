ba7f2093ccee8b307b94e6d34171ae94a12b3752
Mohammad AlSammak <msk170a@gmail.com>
2026-01-13 01:31:36 +0300

    chore: cosmetic changes.

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,4 +1,7 @@
 return {
+  {
+    "mg979/vim-visual-multi",
+  },
   { -- neovim healthcheck thorw an error on me becasue this is not installed.
     "vhyrro/luarocks.nvim",
     priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.

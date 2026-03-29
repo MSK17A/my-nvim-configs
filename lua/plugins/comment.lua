4195b2b76821202aa68967511c4db8b55cce65fe
Mohammad AbdulAziz AlSammak <mohammad@Mohammads-MacBook-Pro.local>
2026-03-29 03:09:27 +0300

    bugfix: context aware comments, to fix tsx commenting // bug.

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,13 @@
 return {
-  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
-  "numToStr/Comment.nvim",
-  config = function()
-    require("Comment").setup()
-  end,
+  {
+    "numToStr/Comment.nvim",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
+  },
 }
